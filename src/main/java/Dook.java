8ba95ee94a1ee763590ac54d6a7970ea9d25c2b6
Dung <dangleha3008@gmail.com>
2026-08-27 21:07:23 +0800

    Update banner

--- src/main/java/Dook.java
+++ src/main/java/Dook.java
@@ -1,18 +1,38 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class Dook {
+    public static final String BANNER = """
+
+                                            ▓█████▄  ▒█████   ▒█████   ██ ▄█▀
+                                            ▒██▀ ██▌▒██▒  ██▒▒██▒  ██▒ ██▄█▒ 
+                                            ░██   █▌▒██░  ██▒▒██░  ██▒▓███▄░ 
+                                            ░▓█▄   ▌▒██   ██░▒██   ██░▓██ █▄ 
+                                            ░▒████▓ ░ ████▓▒░░ ████▓▒░▒██▒ █▄
+                                             ▒▒▓  ▒ ░ ▒░▒░▒░ ░ ▒░▒░▒░ ▒ ▒▒ ▓▒
+                                             ░ ▒  ▒   ░ ▒ ▒░   ░ ▒ ▒░ ░ ░▒ ▒░
+                                             ░ ░  ░ ░ ░ ░ ▒  ░ ░ ░ ▒  ░ ░░ ░ 
+                                               ░        ░ ░      ░ ░  ░  ░   
+                                             ░                               
+                                        """;
+    public static final String BAR = "_".repeat(48) + "\n";
+    public static final String[] GREETINGS = {
+        "The shadows just got a little colder.",
+        "You should not have opened this chat.",
+        "Are you alone in the room right now?"
+    };
+
+    public static void printDookMsg(String msg) {
+        System.out.print(BAR + msg + "\n\n> ");
+    }
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        String banner = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        String chat_bar = "____________________________________________________________\n";
+        Random random = new Random();
 
-        System.out.println(banner);
-        System.out.print(chat_bar + "Hello! I'm Dook.\nWhat can I do for you?\n\n> ");
+        System.out.println(BANNER);
+        printDookMsg("I am Dook.\n" + GREETINGS[random.nextInt(GREETINGS.length)]);
         in.nextLine();
-        System.out.println(chat_bar + "Bye. Hope to see you again soon!");
+        System.out.println(BAR + "Goodbye for now.");
     }
 }
