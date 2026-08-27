a70f79e7ae131e744879619f63d97eaa7e4d83db
Dung <dangleha3008@gmail.com>
2026-08-27 21:28:09 +0800

    Add support for list

--- src/main/java/Dook.java
+++ src/main/java/Dook.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -29,13 +30,23 @@ public class Dook {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         Random random = new Random();
+        ArrayList<String> tasks = new ArrayList<>();
 
         System.out.print(BANNER);
         printDookMsg("I am Dook.\n" + GREETINGS[random.nextInt(GREETINGS.length)]);
 
         String userQuery = in.nextLine();
         while (!userQuery.equals("bye")) {
-            printDookMsg(userQuery);
+            if (userQuery.equals("list")) {
+                String msg = "";
+                for (int i = 0; i < tasks.size(); i++) {
+                    msg += (i+1) + ". " + tasks.get(i) + "\n";
+                }
+                printDookMsg(msg);
+            } else {
+                tasks.add(userQuery);
+                printDookMsg("added: " + userQuery);
+            }
             userQuery = in.nextLine();
         }
         System.out.println("\nGoodbye for now.");
