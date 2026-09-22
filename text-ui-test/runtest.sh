a0777a872f8bf2255c86c86936cb107b9d31bb44
Dung <dangleha3008@gmail.com>
2026-09-22 15:39:16 +0800

    Add github action CI

--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -1,29 +1,22 @@
-if [ -e "../bin" ]
-then
-    rm -rf ../bin
-fi
-mkdir ../bin
+#!/usr/bin/env bash
+set -euo pipefail
 
-if [ -e "./ACTUAL.txt" ]
-then
-    rm ACTUAL.txt
-fi
+rm -rf ../bin
+mkdir ../bin
+rm -f ACTUAL.txt
 
 find .. -name "*.java" > sources.txt
-if ! javac -Xlint:none -d ../bin @sources.txt
-then
+if ! javac -Xlint:none -d ../bin @sources.txt; then
     echo "********** BUILD FAILURE **********"
-    read -n 1 -s -r -p "Press any key to continue..."
     exit 1
 fi
 rm sources.txt
 
 java -cp ../bin dook/Dook 412 . < input.txt > ACTUAL.txt
-diff -u ACTUAL.txt EXPECTED.txt
-if [ $? -eq 0 ]
-then
+
+if diff -u ACTUAL.txt EXPECTED.txt; then
     echo "Test result: PASSED"
 else
     echo "Test result: FAILED"
+    exit 1
 fi
-read -n 1 -s -r -p "Press any key to continue..."
